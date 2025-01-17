a6646483b392e000e0a5076664d40593c63dd44f
Igor de Freitas Cruz <igor.freitas.cruz@icloud.com>
2025-01-17 12:00:32 -0300

    config: setting pool related parameters to avoid a database connection reset by peer error

--- src/database.py
+++ src/database.py
@@ -1,10 +1,13 @@
 from contextlib import contextmanager
 
+import pyodbc
 from sqlmodel import Session, create_engine
 
 from src.settings import Settings
 
 settings = Settings()
+
+pyodbc.pooling = False
 
 connection_string = 'mssql+pyodbc://{}:{}@{}/{}?TrustServerCertificate=yes&driver=ODBC+Driver+18+for+SQL+Server'.format(
     settings.MSSQL_USER,
@@ -13,7 +16,7 @@ connection_string = 'mssql+pyodbc://{}:{}@{}/{}?TrustServerCertificate=yes&drive
     settings.APPLICATION_DB,
 )
 
-engine = create_engine(connection_string)
+engine = create_engine(connection_string, pool_recycle=3600)
 
 def get_session():
     with Session(engine, expire_on_commit=True) as session:
